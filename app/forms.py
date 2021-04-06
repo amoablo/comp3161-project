ba39da5944c988a4001effecfcfa6765b26ab190
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-04-05 19:38:50 -0500

    Added login feature, needs to switch buttons

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField, SelectField
+from wtforms import StringField, PasswordField, TextAreaField, SelectField, IntegerField
 from wtforms.validators import InputRequired, DataRequired, Email
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class MealPlanForm(FlaskForm):
+    calories = IntegerField("Calories", validators=[DataRequired()], description="Number of calories\
+         for the week")
