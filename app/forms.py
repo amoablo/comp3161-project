925dc33614de4ca43817e26d4f7d4bb2017ac291
Toya <toyapinnock@gmail.com>
2021-04-06 00:39:44 -0400

    update

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, TextAreaField, SelectField
+from wtforms import StringField, PasswordField, TextAreaField, SelectField, IntegerField
 from wtforms.validators import InputRequired, DataRequired, Email
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
