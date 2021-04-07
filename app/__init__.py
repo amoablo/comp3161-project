0348fb11822d584a1bcf8dd668bf83d0066fa34a
Phillipj1514 <phillipjunior95@gmail.com>
2021-04-07 03:16:56 -0500

    merges changes over

--- app/__init__.py
+++ app/__init__.py
@@ -13,15 +13,19 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "GqAExVdKcc66GNtuceYYSUSkJ3bhsULZhQZtT2xDrrAtz6KG4M6Xm"
 
-# Setup SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = ""
-app.config['DATABASE_HOST'] = os.environ.get('DATABASE_HOST')
-app.config['DATABASE_USER'] = os.environ.get('DATABASE_USER')
-app.config['DATABASE_PASSWORD'] = os.environ.get('DATABASE_PASSWORD')
-app.config['DATABASE_NAME'] = os.environ.get('DATABASE_NAME')
+# Setup Database
+# app.config['DATABASE_HOST'] = os.environ.get('DATABASE_HOST')
+# app.config['DATABASE_USER'] = os.environ.get('DATABASE_USER')
+# app.config['DATABASE_PASSWORD'] = os.environ.get('DATABASE_PASSWORD')
+# app.config['DATABASE_NAME'] = os.environ.get('DATABASE_NAME')
+
+app.config['DATABASE_HOST'] = "localhost"
+app.config['DATABASE_USER'] = "comp3161-project2"
+app.config['DATABASE_PASSWORD'] = "comp3161-project2"
+app.config['DATABASE_NAME'] = "comp3161_project2_mealplanner"
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-app.config['UPLOAD_FOLDER'] = './uploads'
+app.config['UPLOAD_FOLDER'] = './app/static/uploads/images/recipe'
 
 # Connect to mysql database
 db = mysql.connect(host= app.config['DATABASE_HOST'], 
